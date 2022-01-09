9eae1c53ad6f79a5021d7e2f1a4593c40c8fb5a5
Sorbon Imomnazarov <isorbon@gmail.com>
2022-01-09 16:35:52 +0200

    Added homework Class 01

---
+++ src/class01/homework/homework.java
@@ -0,0 +1,21 @@
+package class01.homework;
+
+import org.testng.annotations.Test;
+
+public class homework {
+
+    @Test
+    public void thirdTest() {
+        System.out.println("I am third test");
+    }
+
+    @Test
+    public void secondTest() {
+        System.out.println("I am second test");
+    }
+
+    @Test
+    public void firstTest() {
+        System.out.println("I am first test");
+    }
+}
